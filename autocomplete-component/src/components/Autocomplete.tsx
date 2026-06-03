517dffd5a58442dd1632cd249ad1b3e00347e42c
GrucaCode <basiag2001@gmail.com>
2026-06-04 01:20:58 +0200

    create generic autocomplete logic

--- autocomplete-component/src/components/Autocomplete.tsx
+++ autocomplete-component/src/components/Autocomplete.tsx
@@ -9,6 +9,7 @@ type AutocompleteProps<T> = {
 
 function Autocomplete<T>({ suggestions, getSuggestionLabel, getSuggestionId }: AutocompleteProps<T>) {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [query, setQuery] = useState('');
     const autocompleteRef = useRef < HTMLDivElement | null > (null);
 
     useEffect(() => {
@@ -32,17 +33,33 @@ function Autocomplete<T>({ suggestions, getSuggestionLabel, getSuggestionId }: A
         setIsDropdownOpen(true)
     }
 
-    const handleInputChange = () => {
-
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value;
+      setQuery(value);
     }
+
+    const filteredSuggestions =
+      query === ""
+        ? suggestions
+        : suggestions.filter((suggestion) =>
+        getSuggestionLabel(suggestion)
+          .toLowerCase()
+          .includes(query.toLowerCase())
+    );
 
     return (
         <div ref={autocompleteRef} className="relative bg-gray-300 flex flex-col min-w-[100px] w-3/4 max-w-[600px] rounded-sm px-10 py-10" >
             <h2 className="pb-5">Choose options:</h2>
             <div>
-                <input type="text" onFocus={handleInputFocus} onChange={handleInputChange} className="bg-white rounded-sm w-full mb-2 px-5 py-2" />
+                <input type="text" value={query} placeholder="Write here" onFocus={handleInputFocus} onChange={handleInputChange} className="bg-white rounded-sm w-full mb-2 px-5 py-2" />
             </div>
-            {isDropdownOpen && <SuggestionsDropdown />}
+            {isDropdownOpen && (
+              <SuggestionsDropdown 
+                suggestions={filteredSuggestions}
+                getSuggestionLabel={getSuggestionLabel}
+                getSuggestionId={getSuggestionId}
+              />
+            )}
         </div>
     )
 }
--- autocomplete-component/src/components/SuggestionsDropdown.tsx
+++ autocomplete-component/src/components/SuggestionsDropdown.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+type SuggestionsDropdownProps<T> = {
+  suggestions: T[];
+  getSuggestionLabel: (suggestion: T) => string;
+  getSuggestionId: (suggestion: T) => string;
+};
 
-const SuggestionsDropdown: React.FC = () => {
+function SuggestionsDropdown<T>({ 
+    suggestions, 
+    getSuggestionLabel, 
+    getSuggestionId
+}: SuggestionsDropdownProps<T>) {
+    if (suggestions.length === 0) {
+        return (
+            <p className="bg-white mb-2 px-5 py-2">
+                No matching options
+            </p>
+        );
+    };
     return (
         <ul className="bg-white mb-2 px-5 py-2">
-            <p>test</p>
+            {suggestions.map((suggestion) => (
+                <li key={getSuggestionId(suggestion)}>
+                    <button>{getSuggestionLabel(suggestion)}</button>
+                </li>
+            ))}
         </ul>
     );
 }
